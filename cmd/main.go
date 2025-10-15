38c904bea5600c806cd2e7386cb02eb04ffdc72b
Kiiya <a28894@gmail.com>
2025-10-15 16:43:52 +0200

    - Add config.json to start the server based on config.json settings

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var redisClient *redis.Client
+var redisChannel = "websocket_broadcast"
+
+func main() {
+	fmt.Println("Starting server...")
+	redisAddr := os.Getenv("REDIS_ADDR")
+	if redisAddr == "" {
+		redisAddr = "localhost:6379"
+	}
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+	redisDBStr := os.Getenv("REDIS_DB")
+	redisDB := 0
+	if redisDBStr != "" {
+		if db, err := strconv.Atoi(redisDBStr); err == nil {
+			redisDB = db
+		}
+	}
+
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: redisPassword,
+		DB:       redisDB,
+	})
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module TetriON.WebServer
 go 1.25.3
 
 require (
-    github.com/go-chi/chi/v5 v5.9.1
-    github.com/redis/go-redis/v9 v9.9.0
-    nhooyr.io/websocket v1.9.10
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/redis/go-redis/v9 v9.14.0
+	nhooyr.io/websocket v1.8.17
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 )
