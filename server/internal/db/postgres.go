c2b82838e0ea256dded975844c00852b744782bb
Kiiya <a28894@gmail.com>
2025-10-22 19:22:01 +0200

    Update postgres.go

--- server/internal/db/postgres.go
+++ server/internal/db/postgres.go
@@ -1,1 +1,37 @@
 package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"TetriON.WebServer/server/internal/config"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var DB *pgxpool.Pool
+
+func InitPostgres() {
+	cfg := config.GetConfig().Postgres
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%d/%s",
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.DBName,
+	)
+	var err error
+	DB, err = pgxpool.New(context.Background(), dsn)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	// Test the connection
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = DB.Ping(ctx)
+	if err != nil {
+		log.Fatalf("Unable to ping the database: %v\n", err)
+	}
+	log.Println("Connected to PostgreSQL database successfully.")
+}
