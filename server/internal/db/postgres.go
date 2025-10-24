91a59b48528e72d02dcc9c24d7e9b83bcbaf112c
Kiiya <a28894@svalero.com>
2025-10-24 12:30:00 +0200

    - Added new functiosn to obtain data from database

--- server/internal/db/postgres.go
+++ server/internal/db/postgres.go
@@ -9,6 +9,8 @@ import (
 
 	"TetriON.WebServer/server/internal/config"
 	"TetriON.WebServer/server/internal/logging"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -21,14 +23,13 @@ func Init() {
 		logging.LogWithTime(logging.Red, "ERROR", "❌ Invalid port number: %v", err)
 	}
 	dsn := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
-		config.GetEnv(config.ENV_POSTGRES_USER),   
+		config.GetEnv(config.ENV_POSTGRES_USER),
 		config.GetEnv(config.ENV_POSTGRES_PASSWORD),
-		config.GetEnv(config.ENV_POSTGRES_HOST),    
+		config.GetEnv(config.ENV_POSTGRES_HOST),
 		port,
 		config.GetEnv(config.ENV_POSTGRES_DBNAME),
 		config.GetEnv(config.ENV_POSTGRES_SSLMODE),
 	)
-	logging.LogWithTime(logging.White, "INFO", "📡 Connecting to PostgreSQL at: %s", dsn)
 	DB, err = pgxpool.New(context.Background(), dsn)
 	if err != nil {
 		logging.LogWithTime(logging.Red, "ERROR", "❌ Unable to connect to database: %v", err)
@@ -43,4 +44,41 @@ func Init() {
 		return
 	}
 	logging.LogWithTime(logging.Green, "INFO", "✅ Connected to PostgreSQL database successfully.")
+}
+
+func Close() {
+	if DB != nil {
+		DB.Close()
+		logging.LogWithTime(logging.White, "INFO", "🔒 PostgreSQL database connection closed.")
+	}
+}
+
+func GetData(query string, args ...interface{}) (pgx.Rows, error) {
+	return DB.Query(context.Background(), query, args...)
+}
+
+func ExecCommand(command string, args ...interface{}) (pgconn.CommandTag, error) {
+	return DB.Exec(context.Background(), command, args...)
+}
+
+func GetRow(query string, args ...interface{}) pgx.Row {
+	return DB.QueryRow(context.Background(), query, args...)
+}
+
+func GetColumn(query string, args ...interface{}) ([]interface{}, error) {
+	rows, err := DB.Query(context.Background(), query, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var results []any
+	for rows.Next() {
+		var value interface{}
+		if err := rows.Scan(&value); err != nil {
+			return nil, err
+		}
+		results = append(results, value)
+	}
+	return results, nil
 }
